4cd0987938582f0f346846e6f273cfee4e54c2d3
FaisalWant <heliconrngr@gmail.com>
2022-11-07 13:59:17 +0530

    Ran test - function based View

--- website/testapp/urls.py
+++ website/testapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
 
 
-from .views import Pong 
+# from .views import Pong 
 
+from .views import pong 
 
 urlpatterns=[
 
-	path("ping/", Pong.as_view(), name="ping")
+	# path("ping/", Pong.as_view(), name="ping")
+	path("ping/", pong, name="ping")
 
 	]
--- website/testapp/views.py
+++ website/testapp/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.views.decorators.http import require_http_methods 
+
 # Create your views here.
 
 
 """ views for testapp"""
 
-from django.views import View 
+# from django.views import View 
 
-class Pong(View): 
-	""" Respond to ping request""" 
+# class Pong(View): 
+# 	""" Respond to ping request""" 
 
-	def get(self, request): 
-		"""" handle get request""" 
+# 	def get(self, request): 
+# 		"""" handle get request""" 
 
-		return HttpResponse("pong")
+# 		return HttpResponse("pong")
+
+
+""" Function based view"""
+
+@require_http_methods(["GET", "HEAD", "OPTIONS"])
+
+def pong(request): 
+	""" Respond to ping"""
+	if request.method in ["GET", "HEAD"]: 
+
+		return HttpResponse("pong") 
+
+	else: 
+		response= HttpResponse()
+		response["Allow"]= ", ".join(['GET' ,'HEAD', 'OPTIONS'])
+		return response 
