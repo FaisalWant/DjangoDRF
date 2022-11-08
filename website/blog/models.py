c7557298b8ce6fefb5bb16aa9ab090248c0ad626
FaisalWant <heliconrngr@gmail.com>
2022-11-08 20:17:00 +0530

    added models Post, Tag, NewsLink, Startup

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from organizer.models import Startup, Tag 
+from django.db.models import (
+	CharField, 
+	DateField, 
+	ManyToManyField, 
+	Model, 
+	SlugField, 
+	TextField
+	)
 # Create your models here.
+class Post(Model): 
+
+
+	title= CharField(max_length=63) 
+	slug= SlugField(max_length= 63)
+
+	text= TextField() 
+	pub_date= DateField() 
+	tags= ManyToManyField(Tag)
+	startups= ManyToManyField(Startup)
+
+	class Meta: 
+		get_latest_by= "pub_date" 
+		ordering= ["-pub_date", "title"]
+		verbose_name= "blog post" 
+
+
+	def __str__(self): 
+		date_string= self.pub_date.strftime("%Y-%m-%d")
+		return f"{self.title} on {date_string}"
+	
---
+++ website/organizer/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model): 
+	name= models.CharField(max_length=31,
+		unique= True)
+	slug= models.SlugField(max_length=31, 
+		unique= True, 
+		help_text= "A label for URL config") 
+
+	class Meta:
+		ordering=["name"]
+
+	def __str__(self):
+		return self.name
+
+
+
+
+class Startup(models.Model): 
+	name= models.CharField(max_length=31, db_index=True) 
+	slug= models.SLugField(max_length=31, unique= True, help_text="A label for URL config") 
+	description= models.TextField()
+	founded_date= models.DateField("date founded") 
+	contact= models.EmailField() 
+	website= models.URLField(max_length=255) 
+	tags= models.ManyToManyField(Tag) 
+
+	class Meta: 
+		get_latest_by= "founded_date" 
+
+		ordering= ['name']
+	
+	def __str__(self): 
+		return self.name 
+
+
+
+
+
+class NewsLink(models.Model): 
+	title= models.CharField(max_length=31)
+	slug= models.SlugField(max_length=31)
+	pub_date= models.DateField("date published") 
+	link= models.URLField(max_length=255) 
+	startup= models.ForeignKey(Startup, on_delete=models.CASCADE)
+	class Meta: 
+		get_latest_by= "pub_date" 
+		ordering= ["-pub_date"]
+		unique_together= ("slug", "startup")
+		verbose_name= "news article"
+
+	def __str__(self): 
+		return f"{self.startup}: {self.title}"
