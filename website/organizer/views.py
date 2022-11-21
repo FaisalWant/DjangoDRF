8cffddba2698cac5e0f3f33a12239b7a56f34af5
FaisalWant <heliconrngr@gmail.com>
2022-11-21 19:21:24 +0530

    fnc view with http-decorator for organizer app

--- website/organizer/views.py
+++ website/organizer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse 
+from django.views.decorators.http import(
+	require_http_methods)
+
+
+
+
+@require_http_methods(["GET", "HEAD"])
+def hello_world(request): 
+	return HttpResponse("HelloWorld!")
--- website/website/urls.py
+++ website/website/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from testapp import urls as testapp_urls 
-
+from organizer import urls as organizer_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(testapp_urls))
+    path("", include(testapp_urls)),
+    path("org", include(organizer_urls))
 ]
