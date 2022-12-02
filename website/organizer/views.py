c46d2280325c770f1f4ebccb102030a24f65f26f
FaisalWant <heliconrngr@gmail.com>
2022-12-02 16:51:12 +0530

    remove HttpResponse from view and including render

--- website/organizer/views.py
+++ website/organizer/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from django.http import HttpResponse
+
 from django.shortcuts import get_object_or_404
 
-from django.template import loader
+
 from django.views.decorators.http import require_safe 
 
 from .models import Tag,Startup
@@ -27,10 +27,9 @@ from .serializers import TagSerializer, StartupSerializer
 def tag_list(request): 
 	""" Render an HTML template """
 	tag_list= Tag.objects.all()
-	template= loader.get_template("tag/list.html") 
 	context= {"tag_list": tag_list}
-	html_context= template.render(context) 
-	return HttpResponse(html_content) 
+	
+	return render(request, "tag/list.html", context) 
 
 
 
@@ -38,10 +37,10 @@ def tag_list(request):
 def tag_detail(request, slug): 
 	""" Render an HTML template""" 
 	tag= get_object_or_404(Tag, slug=slug) 
-	template= loader.get_template("tag/detail.html") 
+	
 	context= {"tag": tag} 
-	html_content= template.render(context) 
-	return HttpResponse(html_content) 
+	
+	return render(request, "tag/detail.html",context) 
 
 
 
