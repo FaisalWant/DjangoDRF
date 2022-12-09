d798319e9c1c0fa13d16d5772726852b1ece3923
FaisalWant <heliconrngr@gmail.com>
2022-12-09 17:20:38 +0530

    TagAPIDetail inherits RetrieveUpdateAPIView

--- website/organizer/views.py
+++ website/organizer/views.py
@@ -24,11 +24,9 @@ from .serializers import TagSerializer, StartupSerializer
 
 from django.views.generic import DetailView, ListView
 
-from rest_framework.response import Response
 
 from rest_framework.status immport(
-	HTTP_200_OK,
-	HTTP_400_BAD_REQUEST,
+	RetrieveUpdateAPIView
 	)
 
 
@@ -63,46 +61,13 @@ class TagDetail(DetailView):
 # DRF-------------------------------------
 
 
-class TagApiDetail(RetrieveAPIView): 
+class TagApiDetail(RetrieveUpdateAPIView): 
 
 	queryset= Tag.objects.all()
 	serializer_class= TagSerializer
 	lookup_field="slug" 
 
-	def put(self, request, slug): 
-		""" update existing Tag upon PUT""" 
-		tag= self.get_object()
-		s_tag= self.serializer_class(
-			tag, 
-			data=request.data, 
-			context={"request": request},
-			)
-		if s_tag.is_valid():
-			s_tag.save()
-			return Response(s_tag.data, status=HTTP_200_OK) 
-
-		return Response(
-			s_tag.errors, status=HTTP_400_BAD_REQUEST)
-
 	
-
-	def patch(self, request, slug):
-		""" update existing Tag upon PATCH""" 
-		tag= self.get_object()
-		s_tag= self.serializer_class(
-			tag, 
-			data= request.data, 
-			partial= True, 
-			context= {"request": request},
-			)
-
-		if s_tag.is_valid(): 
-			s_tag.save()
-			return Response(s_tag.data, status= HTTP_200_OK)
-
-		return Response(
-			s_tag.errors, status= HTTP_400_BAD_REQUEST
-			)
 
 
 class TagApiList(ListCreateAPIView): 
