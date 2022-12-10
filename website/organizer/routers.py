76dce5f3b5c293d9b164386eb792d03b219e10d6
FaisalWant <heliconrngr@gmail.com>
2022-12-11 01:37:18 +0530

    replace manual viewset map with Router

--- website/organizer/routers.py
+++ website/organizer/routers.py
@@ -2,33 +2,27 @@ from django.urls import path
 
 
 from .views import (
+	
 	TagApiDetail, 
 	TagApiList,
 	StartupApiDetail, 
-	StartupApiList )
+	StartupApiList 
+	NewsLinkAPIDetail, 
+	NewsLinkAPIList
+	 )
 
 
 from .viewsets import TagViewSet 
 
-
-tag_create_list= TagViewSet.as_view(
-	{
-
-	"get": "list",
-	"post": "create"
-
-	})
+api_router= SimpleRouter()
+api_router.register("tag", TagViewSet, base_name="api-tag") 
+api_routes = api_router.urls 
 
 
-tag_retrieve_update_delete = TagViewSet.as_view(
-	{
-	"get": "retrieve",
-	"put": "update",
-	"patch": "partial_update",
-	"delete": "delete"
-	})
 
-urlpatterns=[
+
+
+urlpatterns= api_routes+ [
 
 path("<str:slug>/", TagApiDetail.as_view(), name="api-tag-detail"),
 path("startup/<str:slug>/", StartupApiDetail.as_view(), name="api-startup-detail"),
