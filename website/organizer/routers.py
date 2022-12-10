4e3c64e014c829b3df4db3b654153c7a9b68ea19
FaisalWant <heliconrngr@gmail.com>
2022-12-10 23:54:48 +0530

    replace Tag API views with ViewSet

--- website/organizer/routers.py
+++ website/organizer/routers.py
@@ -8,13 +8,32 @@ from .views import (
 	StartupApiList )
 
 
+from .viewsets import TagViewSet 
 
+
+tag_create_list= TagViewSet.as_view(
+	{
+
+	"get": "list",
+	"post": "create"
+
+	})
+
+
+tag_retrieve_update_delete = TagViewSet.as_view(
+	{
+	"get": "retrieve",
+	"put": "update",
+	"patch": "partial_update",
+	"delete": "delete"
+	})
 
 urlpatterns=[
 
 path("<str:slug>/", TagApiDetail.as_view(), name="api-tag-detail"),
 path("startup/<str:slug>/", StartupApiDetail.as_view(), name="api-startup-detail"),
-path("", TagApiList.as_view(), name="api-tag-list"),
+path("tag/", tag_create_list, name="api-tag-list"),
+path("tag/<str:slug>/", tag_retrieve_update_delete, name="api-tag-detail"),
 path("startup", StartupApiList.as_view(), name="api-startup-list")
 
 ]
