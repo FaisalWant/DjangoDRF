3e6e4b8f245e40d991938d587756c98d8b2885a0
FaisalWant <heliconrngr@gmail.com>
2022-12-15 18:13:01 +0530

    Implemented Mixin for Forms

--- website/organizer/forms.py
+++ website/organizer/forms.py
@@ -7,31 +7,33 @@ from django.core.exceptions import ValidationError
 
 from .models import Tag 
 
-class TagForm(Form): 
+
+class LowercaseNameMixin: 
+	def clean_name(self): 
+		return self.cleaned_data["name"].lower() 
+
+class SlugCleanMixin: 
+	def clean_slug(self): 
+
+		slug= self.cleaned_data['slug'] 
+		if slug == "create":
+			raise ValidationError(
+				"Slug may not be 'create'."
+				)
+
+		return 
+
+
+class TagForm(LowercaseNameMixin, ModelForm): 
 	""" HTML form for Tag objects""" 
 
-	name= CharField(max_length=31) 
-	slug= SlugField(
-		help_text="A label for Url Config", 
-		max_length=31, 
-		required= False, 
-		)
+	class Meta: 
+		model= Tag 
+		fields = "__all__"
 
 
-	def clean_name(self): 
-		return self.cleaned_data['name'].lower() 
-
-
-	def clean)slug(self): 
-		slug= self.cleaned_data["slug"] 
-		if slug= "create":
-			raise ValidationError("Slug may not be 'create'")
-
-		return slug 
-
-	def save(self): 
-		""" Save the data in the bound form""" 
-		return Tag.objects.create(
-			name= self.cleaned_data["name"], 
-			slug= self.cleaned_data["slug"],
-			)
+	
+class StartupForm(LowercaseNameMixin, SlugCleanMixin, ModelForm): 
+	class Meta:
+		model= Startup 
+		fields= "__all__" 
