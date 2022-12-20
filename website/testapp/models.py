a329ad40669f903ab09c694151ac34046e8fbfe3
FaisalWant <heliconrngr@gmail.com>
2022-12-20 23:59:01 +0530

    added tag model for testapp

--- website/testapp/models.py
+++ website/testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django_extensions.db.fields import AutoSlugField 
+
+
+class Tag(Model): 
+	name = CharField(max_length=31) 
+	slug= AutoSlugField(
+		help_text= "A label for URL config", 
+		max_length= 31, 
+		populate_from=["name"]
+
+		)
 # Create your models here.
