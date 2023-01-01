ee688a13b5dfc7346dfce2c849f103e859e92644
FaisalWant <heliconrngr@gmail.com>
2023-01-02 01:41:28 +0530

    Subclass PasswordChangeView to notify user

--- website/user/views.py
+++ website/user/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.views import (
+	PasswordChangeView as BasePasswordChangeView,
+	)
 
-# Create your views here.
+from django.contrib.messages import success 
+from django.urls import reverse_lazy
+
+
+class SuccessMessageMixin: 
+	""" Notify user of success after submitting a form""" 
+
+	success_message= "Success!"
+
+	def form_valid(self, form): 
+		""" When form is valid: notify user of success""" 
+
+		success(
+			self.request, 
+			self.success_message, 
+			fail_silently=True, 
+			)
+
+		return super().form_valid(form) 
+
+
+
+
+class PasswordChangeView(
+	SuccessMessageMixin, BasePasswordChangeView):
+""" Allow authenticated users to changes password: 
+messages success to user """ 
+
+success_message= "Password Changed Successfully"
+success_url= reverse_lazy("auth:account")
