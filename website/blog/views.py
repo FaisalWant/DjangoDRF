9418cf42da938785506619a60320136e6af7fe0d
FaisalWant <heliconrngr@gmail.com>
2023-01-15 21:02:08 +0530

    Modify views with Permissions

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,77 +1,93 @@
-from django.shortcuts import render
+"""Views for Blog App
 
-from rest_framework.generics import (
-	ListAPIView,
-	RetrieveAPIView,
+http://ccbv.co.uk/projects/Django/2.2/django.contrib.auth.mixins/PermissionRequiredMixin/
+"""
+from django.contrib.auth.mixins import (
+    PermissionRequiredMixin,
+)
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-	)
+from .forms import PostForm
 from .models import Post
 
-from .serializers import (
-	PostSerializer
 
-	)
+class PostObjectMixin:
+    """Django View mix-in to find blog posts"""
 
-from django.views import View
-from django.views.generic import (
-	ListView
-	)
+    model = Post
 
+    def get_object(self, queryset=None):
+        """Get a blog post using year, month, and slug
 
-# Create your views here.
-
-class PostList(ListView):  #Generic List
-	model= Post
-	template_name="post/list.html"
-
-
-class PostDetail(View):    
-
-	def get(self, request, year, month, slug):
-		post=get_object_or_404(
-			Post,
-			pub_date__year=year,
-			pub_date__month=month,
-			slug=slug
-			)
-		return render(
-			request,"post/detail.html",{"post":post}
-
-			)
-
-
-### Django RestFrameWork
-
-class PostAPIList(ListAPIView):
-    """Return data for multiple Post objects"""
-
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostAPIDetail(RetrieveAPIView):
-    """Return data for a single Post object"""
-
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def get_object(self):
-        """Override DRF's generic method
-
-        http://www.cdrf.co/3.7/rest_framework.generics/ListAPIView.html#get_object
+        http://ccbv.co.uk/SingleObjectMixin
         """
-        month = self.kwargs.get("month")
-        year = self.kwargs.get("year")
-        slug = self.kwargs.get("slug")
+        if queryset is None:
+            queryset = self.get_queryset()
 
-        queryset = self.filter_queryset(self.get_queryset())
-
-        post = get_object_or_404(
+        year, month, slug = map(
+            self.kwargs.get, ["year", "month", "slug"]
+        )
+        if any(arg is None for arg in (year, month, slug)):
+            raise AttributeError(
+                f"View {self.__class__.__name__} must be"
+                f"called with year, month, and slug for"
+                f"Post objects"
+            )
+        return get_object_or_404(
             queryset,
             pub_date__year=year,
             pub_date__month=month,
             slug=slug,
         )
-        self.check_object_permissions(self.request, post)
-        return post
 
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    """Create new blog posts"""
+
+    form_class = PostForm
+    model = Post
+    permission_required = "blog.add_post"
+    template_name = "post/form.html"
+    extra_context = {"update": False}
+
+
+class PostDetail(PostObjectMixin, DetailView):
+    """Display a single blog Post"""
+
+    template_name = "post/detail.html"
+
+
+class PostDelete(
+    PermissionRequiredMixin, PostObjectMixin, DeleteView
+):
+    """Delete a single blog post"""
+
+    permission_required = "blog.delete_post"
+    template_name = "post/confirm_delete.html"
+    success_url = reverse_lazy("post_list")
+
+
+class PostList(ListView):
+    """Display a list of blog Posts"""
+
+    model = Post
+    template_name = "post/list.html"
+
+
+class PostUpdate(
+    PermissionRequiredMixin, PostObjectMixin, UpdateView
+):
+    """Update existing blog posts"""
+
+    form_class = PostForm
+    permission_required = "blog.change_post"
+    template_name = "post/form.html"
+    extra_context = {"update": True}
