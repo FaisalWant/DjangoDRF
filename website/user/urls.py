ccc12e98195285e311a36a5fa1bf8626a639801d
FaisalWant <heliconrngr@gmail.com>
2023-01-16 01:04:31 +0530

    Avoid namespace issues: add registration URLs

--- website/user/urls.py
+++ website/user/urls.py
@@ -11,7 +11,7 @@ from django.contrib.auth.views import (
 
 from django.contrib.auth.decorators import login_required 
 
-from django.urls import path, reverse_lazy
+from django.urls import path, reverse_lazy,re_path
 from django.views.generic import TemplateView 
 
 from django_registration.backends.activation import (
@@ -19,12 +19,13 @@ from django_registration.backends.activation import (
 )
 from django_registration.backends.activation.views import (
     RegistrationView,
+    ActivationView
 )
 
 from .forms import RegistrationForm
 
 urlpatterns=[
-	 path("account/", AccountPage.as_view(), name="account"), 
+	path("account/", AccountPage.as_view(), name="account"), 
 	path("login/", LoginView.as_view(template_name="user/login.html"), name="login"),
 	path("logout/",LogoutView.as_view(template_name="user/logout.html"), name="logout"),
 	path(
@@ -37,9 +38,45 @@ urlpatterns=[
 	path("password_reset/", PasswordResetView.as_view(), name= "password_reset"),
 	path("password_reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm",
 		),
-	path("register/",RegistrationView.as_view(form_class=RegistrationForm),
+	path(
+        "activate/complete/",
+        TemplateView.as_view(
+            template_name="django_registration/activation_complete.html"
+        ),
+        name="django_registration_activation_complete",
+    ),
+    re_path(
+        r"^activate/(?P<activation_key>[-:\w]+)/$",
+        ActivationView.as_view(
+            success_url=reverse_lazy(
+                "auth:django_registration_activation_complete"
+            )
+        ),
+        name="django_registration_activate",
+    ),
+    path(
+        "register/",
+        RegistrationView.as_view(
+            form_class=RegistrationForm,
+            success_url=reverse_lazy(
+                "auth:django_registration_complete"
+            ),
+        ),
         name="django_registration_register",
     ),
-    path("", include(registration_urls)),
+    path(
+        "register/complete/",
+        TemplateView.as_view(
+            template_name="django_registration/registration_complete.html"
+        ),
+        name="django_registration_complete",
+    ),
+    path(
+        "register/closed/",
+        TemplateView.as_view(
+            template_name="django_registration/registration_closed.html"
+        ),
+        name="django_registration_disallowed",
+    ),
 
-	]
+]
