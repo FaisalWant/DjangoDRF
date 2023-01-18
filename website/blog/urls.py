ac964bc513f954ee9e30bdbb39b934fa83968ccb
FaisalWant <heliconrngr@gmail.com>
2023-01-18 16:59:08 +0530

    Demonstrate us of generic date views

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -1,14 +1,44 @@
-""" URL paths for Blog App """
+"""URL paths for Blog App"""
+from django.urls import path
 
-from django.urls import path 
+from .views import (
+    PostArchiveMonth,
+    PostArchiveYear,
+    PostCreate,
+    PostDelete,
+    PostDetail,
+    PostList,
+    PostUpdate,
+)
 
-from .views import PostDetail, PostList 
-
-
-urlpatterns =[
-
-	path("", PostList.as_view(), name="post_list"),
-	path("<int:year>/<int:month>/<str:slug>/", PostDetail.as_view(), name= "post_detail")
-
-
+urlpatterns = [
+    path("", PostList.as_view(), name="post_list"),
+    path(
+        "create/", PostCreate.as_view(), name="post_create"
+    ),
+    path(
+        "<int:year>/",
+        PostArchiveYear.as_view(),
+        name="post_archive_year",
+    ),
+    path(
+        "<int:year>/<int:month>/",
+        PostArchiveMonth.as_view(),
+        name="post_archive_month",
+    ),
+    path(
+        "<int:year>/<int:month>/<str:slug>/",
+        PostDetail.as_view(),
+        name="post_detail",
+    ),
+    path(
+        "<int:year>/<int:month>/<str:slug>/delete/",
+        PostDelete.as_view(),
+        name="post_delete",
+    ),
+    path(
+        "<int:year>/<int:month>/<str:slug>/update/",
+        PostUpdate.as_view(),
+        name="post_update",
+    ),
 ]
